59e76f393826a3aca527b31aa01e8854f4a8bf65
Jessica Nelson <yahwehschica@yahoo.com>
2021-11-15 13:00:50 -0500

    commit tree 536953e93f211213c3bca26f58a8804206d6dc08

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code here
+function createEmployeeRecord (name, surname, title, hourlyPay) {
+    const employeeRecord = {
+        firstName: name,
+        familyName: surname,
+        jobTitle: title,
+        payPerHour: hourlyPay,
+        // time in event, timeout event
+    }
+        return employeeRecord
+}
+
+
